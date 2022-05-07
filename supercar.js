e77dc5f0a1f4b1ab4527884c8f0faa958eeb885f
tanluong0503 <nguyentanluong0503@gmail.com>
2022-05-07 22:49:06 +0700

    update

--- supercar.js
+++ supercar.js
@@ -7,15 +7,17 @@ class Product {
     }
 }
 var products = [
-    new Product(1, "Lamborghini", 1000000000, 'lamborghini.jpg'),
-    new Product(2, "MayBach", 3000000000, 'maybach.jpg'),
-    new Product(3, "Mercedes", 1000000000, 'mercedes.jpg'),
-    new Product(4, "Rolls-Royce", 1000000000, 'rollroyce.jpg'),
-    new Product(5, "Rolls-232323", 12312312, 'rollroyce.jpg'),
+    new Product(1, "Lamborghini", 3573000, 'lamborghini.jpg'),
+    new Product(2, "MayBach", 2943000, 'maybach.jpg'),
+    new Product(3, "Mercedes", 2384000, 'mercedes.jpg'),
+    new Product(4, "Rolls-Royce", 2746000, 'rollroyce.jpg'),
+    new Product(5, "Ferrari",3148000, 'ferrari.jpg'),
+    new Product(6, "Mc Laren", 1924000,'mclaren.jpg'),
+    new Product(7, "Porsche",2478000,'porsche.jpg'),
 ]
 // Hàm tạo trang sản phẩm:
 function renderProducts() {
-    
+
     let htmls = products.map(function (product, index) {
         return `
         <div class="content_product">
@@ -24,7 +26,7 @@ function renderProducts() {
             </div>
             <div class="content_product-name">
                 <p id="input_${product.id}"> ${product.name} </p>
-                <p> ${product.price} </p>
+                <p> ${currencyFormat(product.price)} </p>
             </div>
             <div class="action">
             <button type="button" onclick="btnAdd(${product.id})">Thêm</button>
@@ -60,19 +62,23 @@ function btnAdd(id) {
 function btnEdit(pdtId) {
     document.querySelector(".form-edit").classList.remove("edit-none");
     position = pdtId;
-    let pdt = getIdProducts();
-    console.log(pdt.position)
+    let pdt = getIdProducts(pdtId);
+    console.log(pdtId)
     let str = `
     <div>
     <label for="">Nhập tên sản phẩm muốn sửa</label>
     <input type="text" id="editName" value="${pdt.name}"> 
     <label for="editPrice">Nhập giá sản phẩm muốn sửa</label>
-    <input type="number" id="editPrice">
+    <input type="number" id="editPrice" value="${pdt.price}">
     <label for="editImage">Nhập hình ảnh sản phẩm muốn sửa</label>
-    <input type="url" id="editImage">
+    <input type="url" id="editImage" value="${pdt.image}">
+    <div>
+    <button type="button" onclick="btnUpdateEdit(${pdt.id})">Update</button>
+    <button type="button" onclick="clearFormEdit(${pdt.id})">Cancel</button>
+</div>
 </div>
     `
-    document.querySelector('.form-edit').innerHTML = str; 
+    document.querySelector('.form-edit').innerHTML = str;
     // renderProducts();
 }
 
@@ -82,39 +88,68 @@ function clearFormAdd() {
 function clearFormEdit() {
     document.querySelector(".form-edit").classList.add("edit-none");
 }
-function btnUpdateAdd(){
+function btnUpdateAdd(pdtId) {
     let product = sortGetId() + 1;
-    let editName = document.querySelector('#addName').value;
-    let editPrice = document.querySelector('#addPrice').value;
-    let editImage = document.querySelector('#addImage').value;
-    products.name = editName;
-    products.price = editPrice;
-    products.image = editImage;
-    let pdt = new Product(product,editName,editPrice,editImage);
+    let addName = document.querySelector('#addName').value;
+    let addPrice = document.querySelector('#addPrice').value;
+    let addImage = document.querySelector('#addImage').value;
+    products.name = addName;
+    products.price = addPrice;
+    products.image = addImage;
+    let pdt = new Product(product, addName, addPrice, addImage);
     products.push(pdt);
+    clearFormAdd();
+    alertProductName(addName);
+    renderProducts();
+    resetProducts();
+}
+function btnUpdateEdit(pdtId) {
+    let pdt = getIdProducts(pdtId);
+    let editName = document.querySelector('#editName').value;
+    let editPrice = document.querySelector('#editPrice').value;
+    let editImage = document.querySelector('#editImage').value;
+    pdt.name = editName;
+    pdt.price = editPrice;
+    pdt.image = editImage;
+    // document.querySelector('.form-edit').classList.add('edit-none');
+    clearFormEdit();
     renderProducts();
 }
-function btnUpdateEdit(){
-
+function alertProductName() {
+    let reset = document.querySelector('#addName').value;
+    if (reset.trim() == '') {
+        return alert('xin nhap ten')
+    }
 }
-function getIdProducts(pdtId){
-    let product = products.find(function(pdt1){
+function resetProducts() {
+    let pdtId = document.querySelector('#addName').value = '';
+    let addPrice = document.querySelector('#addPrice').value = '';
+    let addImage = document.querySelector('#addImage').value = '';
+}
+function getIdProducts(pdtId) {
+    let product = products.find(function (pdt1) {
         return pdt1.id == pdtId
     })
     return product;
-    // console.log(product)
-    // console.log(pdtId)
-    // return products.find(function(product){
-    //     return product == pdtId;
-    // })
+
 }
-function sortGetId(){
+function sortGetId() {
     let arr = [...products]
-    let getId = arr.sort(function(pdtId1,pdtId2){
+    let getId = arr.sort(function (pdtId1, pdtId2) {
         return pdtId2.id - pdtId1.id
     })[0].id
     console.log(getId)
     return getId;
-        
+
+}
+function currencyFormat(number) {
+
+    return number.toLocaleString("en-US", {
+
+        style: "currency",
+        currency: "USD",
+
+    })
+
 }
 renderProducts();
